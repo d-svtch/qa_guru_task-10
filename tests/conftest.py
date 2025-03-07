c9bc99c288a61b5338e306011196107dc47f2afa
Dmitriy Vasilyev <61234128+d-svtch@users.noreply.github.com>
2025-03-07 14:33:18 +0300

    added .env support

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,9 +3,11 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from utils import attach
+from dotenv import load_dotenv
+import os
 
 
-@pytest.fixture(scope="function", autouse=True)
+@pytest.fixture(scope="function")
 def operations_with_browser():
     options = Options()
     selenoid_capabilities = {
@@ -16,9 +18,15 @@ def operations_with_browser():
             "enableVideo": True
         }
     }
+
+    load_dotenv()
+    selenoid_login = os.getenv("SELENOID_LOGIN")
+    selenoid_pass = os.getenv("SELENOID_PASS")
+    selenoid_url = os.getenv("SELENOID_URL")
+
     options.capabilities.update(selenoid_capabilities)
     driver = webdriver.Remote(
-        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        command_executor=f"https://{selenoid_login}:{selenoid_pass}@{selenoid_url}/wd/hub",
         options=options
     )
 
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -4,7 +4,7 @@ from qa_guru_task8_tests.model.model import RegistrationPage
 from qa_guru_task8_tests.users import User
 
 
-def test_full_complited_form():
+def test_full_complited_form(operations_with_browser):
     student = User(full_name="Ivan Ivanovich", email="Ivanovich@testmail.com", gender="Male",
                    phone_number="7999999999", date_of_birth=date(2005, 5, 28), subjects="English",
                    hobbies=["Sports", "Reading"], picture="images.png", current_address="Test address, 11/1",
